ba25e2759cf306336df6f97178eac4c81e18b4aa
DanielSandoval <danielsandoval182@gmail.com>
2015-08-03 16:06:51 -0600

    Ask word and URLs

--- application.py
+++ application.py
@@ -1,3 +1,24 @@
 # Aqui escribe tu codigo
 
-print "Hello World"
+import os
+import sys
+import urllib2
+import re
+
+def ask_word_url():
+	word = raw_input("Type a word: ")
+	url1 = raw_input("Type the first URL: ")
+	url2 = raw_input("Type the second URL: ")
+
+def clean_screen():
+	os.system('reset')
+
+def exit():
+	sys.exit()
+
+def my_application():
+		clean_screen()
+		ask_word_url()
+		exit()
+
+my_application()
