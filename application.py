e7594a239f5c6f992077e98d752e79e38379fa27
DanielSandoval <danielsandoval182@gmail.com>
2015-08-19 10:53:57 -0600

    Fixed verify link

--- application.py
+++ application.py
@@ -19,11 +19,11 @@ class search_engine(object):
 			self.menu_decision(menu_option)
 		
 	def menu_print(self):
-		print "SEARCH WORD"
-		print "EXIT"
+		print "1.SEARCH WORD"
+		print "2.EXIT"
 
 	def menu_option(self):
-		option_menu = raw_input("Type the option you want: ")
+		option_menu = raw_input("Type the number or option you want do: ")
 		option_menu = self.option_menu_min(option_menu)
 		return option_menu
 
@@ -34,15 +34,15 @@ class search_engine(object):
 
 	#Function 2
 	def verify_option_menu(self, option_menu):
-		if option_menu == "search word" or option_menu == "exit":
+		if option_menu == "search word" or option_menu == "exit" or option_menu == "1" or option_menu == "2":
 			return "Valid option"
 		else:
 			return "Invalid option"
 
 	def menu_decision(self, menu_option):
-		if menu_option == "search word":
+		if menu_option == "search word" or menu_option == "1":
 			self.option_search()
-		elif menu_option == "exit":
+		elif menu_option == "exit" or menu_option == "2":
 			self.exit()
 		else:
 			message = raw_input("Invalid Input")
@@ -52,8 +52,8 @@ class search_engine(object):
 		verify_urls = "Incorrect urls"
 		while verify_urls == "Incorrect urls":
 			word, url1, url2 = self.ask_word_url()
-			page_html1 = self.open_page1(url1, url2)
-			page_html2 = self.open_page2(url1, url2)
+			page_html1 = self.open_page1(url1)
+			page_html2 = self.open_page2(url2)
 			verify_urls = self.verify_urls(page_html1, page_html2)
 		count_page1 = self.count_page1(word, page_html1)
 		count_page2 = self.count_page2(word, page_html2)
@@ -65,31 +65,33 @@ class search_engine(object):
 		url2 = raw_input("Type the second URL: ")
 		return word, url1, url2
 
-	def open_page1(self, url1, url2):
+	def open_page1(self, url1):
 		try:
-			#url1 = "http://%s" % url1
 			page1 = urllib2.urlopen(url1)
 			page_html1 = page1.read()
 			return page_html1
 		except ValueError:
-			#try:
-				#url1 = "http://%s" % url1
-				#page1 = urllib2.urlopen(url1)
-				#page_html1 = page1.read()
-				##page_html1 = self.open_page1(url1, url2)
-				#return page_html1
-			#except ValueError:
-			message = raw_input("You didn't type correctly the first URL")
-			return "Incorrect url"
+			try:
+				url1 = "http://%s" % url1
+				page_html1 = self.open_page1(url1)
+				return page_html1
+			except:
+				message = raw_input("You didn't type correctly the first URL")
+				return "Incorrect url"
 
-	def open_page2(self, url1, url2):
+	def open_page2(self, url2):
 		try:
 			page2 = urllib2.urlopen(url2)
 			page_html2 = page2.read()
 			return page_html2
 		except ValueError:
-			message = raw_input("You didn't type correctly the second URL")
-			return "Incorrect url"
+			try:
+				url2 = "http://%s" % url2
+				page_html2 = self.open_page2(url2)
+				return page_html2
+			except:
+				message = raw_input("You didn't type correctly the second URL")
+				return "Incorrect url"
 
 	def verify_urls(self, page_html1, page_html2):
 		if page_html1 == "Incorrect url" or page_html2 == "Incorrect url":
